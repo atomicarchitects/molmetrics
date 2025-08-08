742ce9ac08ccbce2afdbb3c0edf9025999930e1c
Song Kim <songkimwl@gmail.com>
2025-08-08 12:38:28 -0400

    take out synthesizability specifically (can be computed w compute_metric)

--- molmetrics/rdkit/molecules.py
+++ molmetrics/rdkit/molecules.py
@@ -5,7 +5,6 @@ import ase
 from rdkit import Chem
 from rdkit.Chem import rdDetermineBonds
 from rdkit import rdBase
-from rdkit.Contrib.SA_Score import sascorer
 import numpy as np
 
 log = logging.getLogger(__name__)
@@ -64,12 +63,6 @@ class RDKitMolecules:
         valid_mols = self.keep_valid()
         valid_mols = valid_mols.add_bonds()
         return [metric_fn(mol) for mol in valid_mols]
-    
-    def synthesizability(self) -> List[float]:
-        """Computes the synthesizability of valid molecules."""
-        valid_mols = self.keep_valid()
-        valid_mols = valid_mols.add_bonds()
-        return [sascorer.calculateScore(mol) for mol in valid_mols]
 
     def non_identical(self, other: "RDKitMolecules") -> float:
         """Computes the fraction of identical molecules."""
